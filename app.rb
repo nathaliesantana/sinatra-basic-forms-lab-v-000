8b0a8bf0d8d6c387eff7bb9e688dc2985dc26c35
Nathalie Santana <nathalie.scruz@gmail.com>
2018-11-22 06:33:47 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
   end
 
   post '/puppy' do
-    @puppy = Puppy.new(params['name'], params['breed'], params['months_old'])
+    @puppy = Puppy.new(params[:name], params[:breed], params[:months_old])
      binding.pry
 
     erb :display_puppy
